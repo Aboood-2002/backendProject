2aa8e5e2728d4b73aee061ce02971694512683e5
aboood-2002 <aboodahm807@gamil.com>
2025-06-05 14:42:07 +0300

    Updating payment

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { createCheckoutSession, handleWebhook, verifyPayment } = require('../controllers/paymentController');
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middlewares/authMiddleware.js');
 
 router.post('/checkout', protect, createCheckoutSession);
 router.post('/webhook', handleWebhook);
