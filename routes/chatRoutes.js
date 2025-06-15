c39967c9cf11e01fb2982ca66263f598786284dd
aboood-2002 <aboodahm807@gamil.com>
2025-06-16 01:31:51 +0300

    editing chat routes for adding conversation using text and image for formdata

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -6,13 +6,14 @@ const {createChat, getAllChats, addConversation, getConversation, deleteChat} = 
 
 const {protect} = require("../middlewares/authMiddleware");
 
+const {photoUpload} = require("../middlewares/filesUploader")
 
 router.route("/")
       .post(protect,createChat)
       .get(protect,getAllChats)
 
 router.route("/:id")
-      .post(protect,addConversation)
+      .post(protect,photoUpload.single("image"),addConversation)
       .get(protect,getConversation)
       .delete(protect,deleteChat)
 
