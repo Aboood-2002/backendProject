35462b3fa9991074fed2f6b62db98d49c4b0eddd
aboood-2002 <aboodahm807@gamil.com>
2025-06-22 06:18:33 +0300

    edit User model to get all courses user is enrolled in

--- models/User.js
+++ models/User.js
@@ -45,8 +45,19 @@ const userSchema = new mongoose.Schema({
     attemptedAt: Date
   }],
 },{
-    timestamps: true
+    timestamps: true,
+    toJSON : {virtuals : true},
+    toObject : {virtuals : true}
 });
+
+
+// Populate Posts That Belongs To This User when he/she Get his/her Profile
+
+userSchema.virtual("courses",{
+    ref : "Course",
+    foreignField:"user",
+    localField:"_id"
+})
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
