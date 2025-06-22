9149002ef8976ba554720c5429f5d455fcd3aca9
aboood-2002 <aboodahm807@gamil.com>
2025-06-22 04:11:49 +0300

    edit get all courses

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -66,7 +66,7 @@ exports.createCourse = asyncHandler(async (req, res) => {
 
 exports.getAllCourses = asyncHandler(async (req, res) => {
 
-    const courses = await Course.find().populate("instructor")
+    const courses = await Course.find().populate("instructor",["-password"])
     res.status(200).json({
         success : true,
         courses
