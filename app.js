7c9fe79d9308376462eda7f8fac0daa46cdca987
aboood-2002 <aboodahm807@gamil.com>
2025-06-27 13:59:48 +0300

    add security packages for apis

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const { handleWebhook } = require('./controllers/paymentController');
 
 const cors = require("cors")
 
+const xss = require("xss-clean");
+
+const rateLimiting = require("express-rate-limit");
+
+const helmet = require("helmet");
+
+const hpp = require("hpp");
+
 require('dotenv').config();
 
 connectToDB()
@@ -23,6 +31,21 @@ app.post('/webhook',express.raw({ type: 'application/json' }), handleWebhook);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+
+// Security Headers (helmet)
+app.use(helmet());
+
+// Prevent Http Param Pollution
+app.use(hpp());
+
+// Prevent XSS(Cross Site Scripting) Attacks
+app.use(xss());
+
+// Rate Limiting
+app.use(rateLimiting({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max:200,
+}));
 
 //routes
 
